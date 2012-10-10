aabcc1d130fac6eb59182f2491f7ee3cdf8e87cb
daKmoR <thomas.allmer@webteam.at>
2012-10-11 01:45:17 +0200

    [BUGFIX] isValid function needs a parameter

--- Classes/Domain/Validator/XformValidator.php
+++ Classes/Domain/Validator/XformValidator.php
@@ -4,7 +4,7 @@
  *  Copyright notice
  *
  *  (c) 2011 Thomas Allmer <at@delusionworld.com>, WEBTEAM GmbH
- *  
+ *
  *  All rights reserved
  *
  *  This script is part of the TYPO3 project. The TYPO3 project is
@@ -41,26 +41,24 @@ class Tx_Xform_Domain_Validator_XformValidator extends Tx_Extbase_Validation_Val
 	 * if any of these is wrong it dies with a message.
 	 *
 	 */
-	public function isValid() {
+	public function isValid($value) {
 		if (t3lib_div::_GP('name') || t3lib_div::_GP('email') || t3lib_div::_GP('e-mail') || t3lib_div::_GP('phone')) {
 			die('honeypot filled - u sure you are not a bot?');
 		}
-		
+
 		if (t3lib_div::_GP('subject') !== strrev(t3lib_div::_GP('subject2'))) {
 			die('tempared with subject and subject2 - u sure you are not a bot?');
 		}
-		
+
 		if (t3lib_div::getIndpEnv('HTTP_USER_AGENT') == "") {
 			die('no user agent - u sure you are not a bot?');
 		}
-		
+
 		if (!$_COOKIE['fe_typo_user']) {
 			die('no cookies - pls activate cookies for this page');
 		}
-		
+
 		return true;
 	}
-	
-}
 
-?>
+}
